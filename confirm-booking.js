6504d57e847ad12df4c2f30230ebfea67f166c3d
Shiva Adhikari <s2whl6xh@duck.com>
2024-09-27 08:11:05 +0545

    sab update vayo css,js. aba kam garxa css, js le

--- confirm-booking.js
+++ confirm-booking.js
@@ -1,10 +1,5 @@
-// {/* <script> */}
 // Flip the card on "Proceed with Booking"
-document
-  .querySelector(".btn-proceed")
-  .addEventListener("click", function () {
-    document.querySelector(".booking-box").classList.add("flipped");
-  });
-
+document.querySelector(".btn-proceed").addEventListener("click", function () {
+  document.querySelector(".booking-box").classList.add("flipped");
+});
 // No need for back or cancel button to flip the card back
-// {/* </script> */}
